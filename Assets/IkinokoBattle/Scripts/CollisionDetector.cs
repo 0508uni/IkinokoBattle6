7e0941c850313f0913e6c452a53ef786d5d9c289
0508uni <105364694+0508uni@users.noreply.github.com>
2022-09-06 18:31:58 +0900

    敵が追っかけてくる

---
+++ Assets/IkinokoBattle/Scripts/CollisionDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    private void OnTriggerStay(Collider other)
+    {
+        onTriggerStay.Invoke(other);
+    }
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/IkinokoBattle/Scripts/EnemyMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // [SerializeField] private PlayerController _playerController;
+
+    private NavMeshAgent _agent;
+    private RaycastHit[] _raycastHits = new RaycastHit[10];
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+   /* void Update()
+    {
+        _agent.destination = _playerController.transform.position;
+    }*/
+
+    public void OnDetectObject(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            var positionDiff = collider.transform.position - transform.position;
+
+            var distance = positionDiff.magnitude;
+
+            var direction = positionDiff.normalized;
+
+            var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance);
+            Debug.Log("hitCount:" + hitCount);
+
+            if(hitCount == 0)
+            {
+                _agent.isStopped = false;
+                _agent.destination = collider.transform.position;
+            }
+            else
+            {
+                _agent.isStopped = true;
+            }
+        }
+    }
+}
